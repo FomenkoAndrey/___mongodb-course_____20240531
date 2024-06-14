4e15379ceae58b04a7c425f47413412ad8df758d
Alecto <fomenkoandrey1978@gmail.com>
2024-06-14 21:32:05 +0300

    mongosh

---
+++ src/ex.13.mongosh/5-find/index13.js
@@ -0,0 +1,18 @@
+load('../random.js')
+
+db.customers.drop()
+
+db.createCollection('customers')
+
+db.customers.insertMany(getUsers(1000, 10, -1))
+
+const query = [{ scores: { $size: 7 } }]
+
+if (true) {
+  query.push({ firstName: /jo/gi })
+  // ! query → [{ scores: { $size: 7 } }, { firstName: /jo/gi }]
+}
+
+cls
+
+db.customers.find({ $and: query }, { _id: false, firstName: true, scores: true })
